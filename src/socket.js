606815c2438390879b963dbc004fea9b06bccf59
RahulGorai0206 <rahulgorai@gmail.com>
2024-06-01 01:30:28 +0530

    fix cors error

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://production-backend-3olq.onrender.com");
+const socket = io("https://production-backend-3olq.onrender.com", {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd"
+  }
+});
 
 export default socket;
